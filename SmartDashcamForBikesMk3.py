0ae1f65e80e69443bd02db04699e1a3d7fe0f8fa
Richard Audette <richard_audette@yahoo.ca>
2023-01-05 16:11:07 -0500

    Rewrite using Oakcamera SDK. Depth working, DB logging.

--- SmartDashcamForBikesMk3.py
+++ SmartDashcamForBikesMk3.py
@@ -1,254 +1,111 @@
 #!/usr/bin/env python3
 # Running the yolov5s blob based on this code from Luxonis
-# https://github.com/luxonis/depthai-experiments/tree/e12d6a7e2f40d3ada35c03fb7b0176b33efe960b/gen2-yolov5
-# Running the depth perception based on this code from Luxonis
-# https://github.com/luxonis/depthai-experiments/tree/master/gen2-calc-spatials-on-host
+# https://github.com/luxonis/depthai-experiments/tree/master/gen2-yolo/device-decoding
 
-from pickle import FALSE
-import cv2
-import depthai as dai
-from util.functions import non_max_suppression
+from depthai_sdk import OakCamera, ArgsParser, Detections, NNData, DetectionPacket, Visualizer, RecordType
 import argparse
-import time
 import numpy as np
-from util.calc import HostSpatialsCalc
-import math
-from paddleocr import PaddleOCR,draw_ocr
+import cv2 as cv2
+from paddleocr import PaddleOCR
 import logging
+import depthai as dai
+import sqlite3
+from datetime import datetime
+import time
 
-#settings
-fps=2
-
-
-labelMap = [
-    "License_Plate"
-]
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-conf", "--confidence_thresh", help="set the confidence threshold", default=0.3, type=float)
-parser.add_argument("-iou", "--iou_thresh", help="set the NMS IoU threshold", default=0.4, type=float)
-parser.add_argument("-db", "--database", help="Store metadata in DB", action='store_true')
-parser.add_argument("-d", "--desktop", help="Turn on things you want running while testing on desktop", action='store_true')
-
-args = parser.parse_args()
-
-nn_path = 'SDCMk3-LicensePlateModel.blob'
-conf_thresh = args.confidence_thresh
-iou_thresh = args.iou_thresh
-
-nn_shape = 416
+#defaults/constants
+manualfocus=100 #0 to 255
+recordingfolder="./data/"
 
 ocr = PaddleOCR(use_angle_cls=True, lang='en') 
 #silence paddleocr
-'''
-for k,v in  logging.Logger.manager.loggerDict.items()  :
-        print('+ [%s] {%s} ' % (str.ljust( k, 20)  , str(v.__class__)[8:-2]) ) 
-        if not isinstance(v, logging.PlaceHolder):
-            for h in v.handlers:
-                print('     +++',str(h.__class__)[8:-2] )
-'''
 logger = logging.getLogger('root')
 logger.setLevel(logging.WARN)
 
-def draw_boxes(frame, boxes, total_classes):
-    if boxes.ndim == 0:
-        return frame
-    else:
+def decode(nn_data: NNData):
+    layer = nn_data.getFirstLayerFp16()
+    results = np.array(layer).reshape((1, 1, -1, 7))
+    dets = Detections(nn_data)
 
-        # define class colors
-        colors = boxes[:, 5] * (255 / total_classes)
-        colors = colors.astype(np.uint8)
-        colors = cv2.applyColorMap(colors, cv2.COLORMAP_HSV)
-        colors = np.array(colors)
+    for result in results[0][0]:
+        if result[2] > 0.5:
+            dets.add(result[1], result[2], result[3:])
 
-        for i in range(boxes.shape[0]):
-            x1, y1, x2, y2 = int(boxes[i,0]), int(boxes[i,1]), int(boxes[i,2]), int(boxes[i,3])
-            conf, cls = boxes[i, 4], int(boxes[i, 5])
+    return dets
 
-            label = f"{labelMap[cls]}: {conf:.2f}" if "default" in nn_path else f"Class {cls}: {conf:.2f}"
-            color = colors[i, 0, :].tolist()
+def callback(packet: DetectionPacket, visualizer: Visualizer):
+    detections: Detections = packet.img_detections
 
-            frame = cv2.rectangle(frame, (x1, y1), (x2, y2), color, 1)
+    now = datetime.now()
+    detecttime=now.strftime("%Y-%m-%d %H:%M:%f")
 
-            # Get the width and height of label for bg square
-            (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.3, 1)
+    num = len(packet.img_detections.detections)
+    if args['desktop']:
+        imgPreview = packet.frame
+        if num<1:
+            cv2.imshow("Augmented Output", imgPreview)
 
-            # Shows the text.
-            frame = cv2.rectangle(frame, (x1, y1 - 2*h), (x1 + w, y1), color, -1)
-            frame = cv2.putText(frame, label, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255,255,255), 1)
-    return frame
+    for detectedobject in packet.img_detections.detections:
+        cursor.execute("INSERT INTO routelog (sessionid,timestamp) VALUES ('"+str(sessionId)+"','"+detecttime+"')")
+        dbconnection.commit()
+        routelogid=cursor.lastrowid
+        print("x ", str(detectedobject.spatialCoordinates.x) + ",y " + str(detectedobject.spatialCoordinates.y) + ",z " + str(detectedobject.spatialCoordinates.z) )
+        roiData = detectedobject.boundingBoxMapping
+        roi = roiData.roi
+        roi = roi.denormalize(packet.frame.shape[1],packet.frame.shape[0])
+        topLeft = roi.topLeft()
+        bottomRight = roi.bottomRight()
+        imgPlate = packet.frame[int(topLeft.y):int(bottomRight.y), int(topLeft.x):int(bottomRight.x)  ]
+        if args['desktop']:
+            imgPreview = packet.frame
+            cv2.rectangle(imgPreview, (int(topLeft.x),int(topLeft.y)),(int(bottomRight.x),int(bottomRight.y)),(0,255,0),2)
 
-def drawandocr_boxes(frame, boxes, total_classes):
-    if boxes.ndim == 0:
-        return frame
-    else:
+        result = ocr.ocr(imgPlate, cls=True)
+        for index, line in enumerate(result):
+            if line[1][1]>=args['ocr_confidence']:
+                #remove whitespace and punctuation that is sometimes detected in place of the Ontario crown symbol
+                sPlate=''.join(filter(str.isalnum,str(line[1][0])))
+                fConfidence=str(line[1][1])
+                sPaddleResult="paddle " + str(line[1][0]) + " confidence " + str(fConfidence)
+                print(sPaddleResult)
+                cursor.execute("INSERT INTO feature (routelogid,featureplatenumber, featuretypeid, feature_x, feature_y, feature_z,timestamp) VALUES ('"+str(routelogid)+"','"+sPlate+"',1,"+str(detectedobject.spatialCoordinates.x)+","+str(detectedobject.spatialCoordinates.y)+","+str(detectedobject.spatialCoordinates.z)+",'"+detecttime+"')")
+                dbconnection.commit()
+                if args['desktop']:
+                    cv2.putText(imgPreview,sPaddleResult,(int(topLeft.x),int(topLeft.y)+20) , cv2.FONT_HERSHEY_SIMPLEX, .8, (0, 255, 0))
+        if args['desktop']:
+            cv2.imshow("Augmented Output", imgPreview)
 
-        # define class colors
-        colors = boxes[:, 5] * (255 / total_classes)
-        colors = colors.astype(np.uint8)
-        colors = cv2.applyColorMap(colors, cv2.COLORMAP_HSV)
-        colors = np.array(colors)
+parser = argparse.ArgumentParser()
+parser.add_argument("-conf", "--config", help="Trained YOLO json config path", default='model/best.json', type=str)
+parser.add_argument("-iou", "--iou_thresh", help="set the NMS IoU threshold", default=0.4, type=float)
+parser.add_argument("-ocr", "--ocr_confidence", help="set the NMS IoU threshold", default=0.9, type=float)
+parser.add_argument("-norec", "--norecording", help="Disable video", action='store_true')
+parser.add_argument("-dt", "--desktop", help="Turn on things you want running while testing on desktop", action='store_true')
 
-        for i in range(boxes.shape[0]):
-            x1, y1, x2, y2 = int(boxes[i,0]), int(boxes[i,1]), int(boxes[i,2]), int(boxes[i,3])
-            conf, cls = boxes[i, 4], int(boxes[i, 5])
+args = ArgsParser.parseArgs(parser)
 
-            label = f"{labelMap[cls]}: {conf:.2f}" if "default" in nn_path else f"Class {cls}: {conf:.2f}"
-            color = colors[i, 0, :].tolist()
+now = datetime.now()
 
-            frame = cv2.rectangle(frame, (x1, y1), (x2, y2), color, 1)
+dbconnection = sqlite3.connect("SmartDashcamForBikesMk3.sqlite3")
+cursor = dbconnection.cursor()
 
-            # Get the width and height of label for bg square
-            (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.3, 1)
-            result = ocr.ocr(frame[y1:y2, x1:x2], cls=True)
-            for line in result:
-                print("paddle",line[1][0])
-#                print("paddle",line)
-                paddleresult = "paddle " +line[1][0]
-                frame = cv2.putText(frame, paddleresult,(x1, y1+20) , cv2.FONT_HERSHEY_SIMPLEX, .8, (0, 255, 0))
+startTime = now.strftime("%Y%m%d-%H%M%S")
+cursor.execute("INSERT INTO session (description) VALUES ('"+startTime+"')")
+sessionId=cursor.lastrowid
+dbconnection.commit()
 
+mxid=""
+for device in dai.Device.getAllAvailableDevices():
+    mxid=device.getMxId()
+    print(mxid)
 
-            # Shows the text.
-            frame = cv2.rectangle(frame, (x1, y1 - 2*h), (x1 + w, y1), color, -1)
-            frame = cv2.putText(frame, label, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255,255,255), 1)
-    return frame
-
-
-# Start defining a pipeline
-pipeline = dai.Pipeline()
-
-pipeline.setOpenVINOVersion(version = dai.OpenVINO.VERSION_2021_1)
-
-# Define a neural network that will make predictions based on the source frames
-detection_nn = pipeline.createNeuralNetwork()
-detection_nn.setBlobPath(nn_path)
-detection_nn.setNumPoolFrames(4)
-detection_nn.input.setBlocking(False)
-detection_nn.setNumInferenceThreads(2)
-
-cam = pipeline.createColorCamera()
-cam.setPreviewSize(nn_shape,nn_shape)
-cam.setInterleaved(False)
-cam.preview.link(detection_nn.input)
-cam.setFps(fps)
-
-# Create outputs
-xout_rgb = pipeline.createXLinkOut()
-xout_rgb.setStreamName("nn_input")
-xout_rgb.input.setBlocking(False)
-
-detection_nn.passthrough.link(xout_rgb.input)
-xout_nn = pipeline.createXLinkOut()
-xout_nn.setStreamName("nn")
-xout_nn.input.setBlocking(False)
-detection_nn.out.link(xout_nn.input)
-
-# Depth cam setup
-# Define sources and outputs
-monoLeft = pipeline.create(dai.node.MonoCamera)
-monoRight = pipeline.create(dai.node.MonoCamera)
-stereo = pipeline.create(dai.node.StereoDepth)
-# Properties
-monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-stereo.initialConfig.setConfidenceThreshold(255)
-stereo.setLeftRightCheck(True)
-stereo.setSubpixel(False)
-# Linking
-monoLeft.out.link(stereo.left)
-monoRight.out.link(stereo.right)
-xoutDepth = pipeline.create(dai.node.XLinkOut)
-xoutDepth.setStreamName("depth")
-stereo.depth.link(xoutDepth.input)
-xoutDepth = pipeline.create(dai.node.XLinkOut)
-xoutDepth.setStreamName("disp")
-stereo.disparity.link(xoutDepth.input)
-
-
-# Pipeline defined, now the device is assigned and pipeline is started
-with dai.Device(pipeline) as device:
-
-    # Output queues will be used to get the rgb frames and nn data from the outputs defined above
-    q_nn_input = device.getOutputQueue(name="nn_input", maxSize=4, blocking=False)
-    q_nn = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
-
-    start_time = time.time()
-    counter = 0
-#    fps = 1
-    layer_info_printed = False
-    while True:
-        in_nn_input = q_nn_input.get()
-        in_nn = q_nn.get()
-
-        frame = in_nn_input.getCvFrame()
-
-#        print("plate ", in_nn_input.getTimestamp())
-
-
-        layers = in_nn.getAllLayers()
-
-        # get the "output" layer
-        output = np.array(in_nn.getLayerFp16("output"))
-
-        # reshape to proper format
-        cols = output.shape[0]//10647
-        output = np.reshape(output, (10647, cols))
-        output = np.expand_dims(output, axis = 0)
-
-        total_classes = cols - 5
-
-        depthQueue = device.getOutputQueue(name="depth")
-        dispQ = device.getOutputQueue(name="disp")
-        hostSpatials = HostSpatialsCalc(device)
-        y = 200
-        x = 300
-        step = 3
-        delta = 5
-        hostSpatials.setDeltaRoi(delta)
-
-        boxes = non_max_suppression(output, conf_thres=conf_thresh, iou_thres=iou_thresh)
-        boxes = np.array(boxes[0])
-
-        if boxes is not None:
-            frame = drawandocr_boxes(frame, boxes, total_classes)
-        
-        
-        #cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, (255, 0, 0))
-        if args.desktop:
-            cv2.imshow("nn_input", frame)
-
-        in_depthFrame = depthQueue.get()
-        depthFrame = in_depthFrame.getFrame()
-#        print("depth frame")
-#        print(in_depthFrame.getTimestamp().total_seconds())
-#        print("depth ", in_depthFrame.getTimestamp())
-
-
-        # Calculate spatial coordiantes from depth frame
-        spatials, centroid = hostSpatials.calc_spatials(depthFrame, (x,y)) # centroid == x/y in our case
-
-        # Get disparity frame for nicer depth visualization
-        disp = dispQ.get().getFrame()
-        disp = (disp * (255 / stereo.initialConfig.getMaxDisparity())).astype(np.uint8)
-        disp = cv2.applyColorMap(disp, cv2.COLORMAP_JET)
-
-        #text.rectangle(disp, (x-delta, y-delta), (x+delta, y+delta))
-        #text.putText(disp, "X: " + ("{:.1f}m".format(spatials['x']/1000) if not math.isnan(spatials['x']) else "--"), (x + 10, y + 20))
-        #text.putText(disp, "Y: " + ("{:.1f}m".format(spatials['y']/1000) if not math.isnan(spatials['y']) else "--"), (x + 10, y + 35))
-        #text.putText(disp, "Z: " + ("{:.1f}m".format(spatials['z']/1000) if not math.isnan(spatials['z']) else "--"), (x + 10, y + 50))
-
-        # Show the frame
-        if args.desktop:
-            cv2.imshow("depth", disp)
-
-        counter += 1
-        if (time.time() - start_time) > 1:
-            fps = counter / (time.time() - start_time)
-
-            counter = 0
-            start_time = time.time()
-
-        if cv2.waitKey(1) == ord('q'):
-            break
+with OakCamera(args=args) as oak:
+    color = oak.create_camera('color',fps=2,encode='H265')
+    nn = oak.create_nn(args['config'], color, nn_type='yolo', spatial=True)
+    color.node.initialControl.setManualFocus(manualfocus)
+    color.node.initialControl.SceneMode(dai.CameraControl.SceneMode.SPORTS)
+    color.node.initialControl.setAutoFocusMode(dai.RawCameraControl.AutoFocusMode.OFF)
+    if not args['norecording']:
+        oak.record([color.out.encoded], recordingfolder, RecordType.VIDEO)
+    oak.visualize(nn, callback=callback)
+    oak.start(blocking=True)
